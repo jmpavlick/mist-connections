10904fef8b1c6f60b0d729fc148ae4c69a4c9fbc
John Pavlick <jmpavlick@gmail.com>
2019-06-19 22:42:55 -0400

    dare I call this an api

--- application.py
+++ application.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'mist': 'connections'}
+        latitude = request.args.get('latitude')
+        longtitude = request.args.get('longtitude')
+        uri = "https://api.darksky.net/forecast/bacc57dc0842ea7a771f2dd4e337e87e/" + latitude + "," + longtitude
+        response = requests.get(url = uri)
+        return response.json()
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/forecast')
 
 if __name__ == '__main__':
     app.run(debug=True)
